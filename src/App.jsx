bff6344fdf57ca21438feb2b3ec87e89f5c0dbc9
SwayamGos <swayamgoswami989@gmail.com>
2023-07-28 00:10:00 +0530

    Fixed bug where a friend was already added

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,28 @@ import FormAddFriend from "./FormAddFriend";
 import Button from "./button";
 import FormSplitBill from "./FormSplitBill";
 
-const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
-];
+// const initialFriends = [
+//   {
+//     id: 118836,
+//     name: "Clark",
+//     image: "https://i.pravatar.cc/48?u=118836",
+//     balance: -7,
+//   },
+//   {
+//     id: 933372,
+//     name: "Sarah",
+//     image: "https://i.pravatar.cc/48?u=933372",
+//     balance: 20,
+//   },
+//   {
+//     id: 499476,
+//     name: "Anthony",
+//     image: "https://i.pravatar.cc/48?u=499476",
+//     balance: 0,
+//   },
+// ];
 function App() {
-  const [friends, setFriends] = useState(initialFriends);
+  const [friends, setFriends] = useState([]);
   const [friendForm, setFriendForm] = useState(false);
   const [activeFriend, setActiveFriend] = useState({active: false});
   console.log(activeFriend)
